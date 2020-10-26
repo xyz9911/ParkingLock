33fc780daa54f184b0bb97780a1034d441555087
CRLBlb <dhgogoLB@163.com>
2020-10-26 14:21:49 +0800

    Initial Commit

---
+++ pages/wxml/index.js
@@ -0,0 +1,153 @@
+// pages/wxml/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    num:0,
+    isChecked:false,
+    test:2,
+    time: (new Date()).toTimeString(),
+    flag: true,
+    array: [{
+      message: 'foo',
+    }, {
+      message: 'bar'
+    },{
+      message:'ABC'
+    }],
+    person:{
+      name:"张三",
+      age:14,
+      duty:"工人"
+    },
+
+
+    list:[
+      {id:1, name:"liubo"},
+      {id:2, name:"zhaosi"},
+      {id:3, name:"qifa"}
+
+    ],
+
+
+
+    objectArray: [
+      {id: 5, unique: 'unique_5'},
+      {id: 4, unique: 'unique_4'},
+      {id: 3, unique: 'unique_3'},
+      {id: 2, unique: 'unique_2'},
+      {id: 1, unique: 'unique_1'},
+      {id: 0, unique: 'unique_0'},
+    ],
+    numberArray: [1, 2, 3, 4],
+    item: {
+      index: 0,
+      msg: 'this is a template',
+      time: '2016-06-18'
+    }
+  },
+  handleInput(e){
+    this.setData({
+      num: e.detail.value
+    })
+  },
+  handleTap(e){
+    console.log(e)
+    e = e.currentTarget.dataset.test
+    this.setData({
+      num: this.data.num*1 + e
+    })
+  },
+  switch: function(e) {
+    const length = this.data.objectArray.length
+    for (let i = 0; i < length; ++i) {
+      const x = Math.floor(Math.random() * length)
+      const y = Math.floor(Math.random() * length)
+      const temp = this.data.objectArray[x]
+      this.data.objectArray[x] = this.data.objectArray[y]
+      this.data.objectArray[y] = temp
+    }
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addToFront: function(e) {
+    const length = this.data.objectArray.length
+    this.data.objectArray = [{id: length, unique: 'unique_' + length}].concat(this.data.objectArray)
+    this.setData({
+      objectArray: this.data.objectArray
+    })
+  },
+  addNumberToFront: function(e){
+    this.data.numberArray = [ this.data.numberArray.length + 1 ].concat(this.data.numberArray)
+    this.setData({
+      numberArray: this.data.numberArray
+    })
+  },
+  test:function(e){
+    var multiplyBy2 = require('./A')
+    this.data.test= multiplyBy2(4)
+    this.setData({
+      test: this.data.test
+    })
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
